e8db60f7e35e627af0666ea1a8aadb7a5ceb6090
xtgxiso <124960772@qq.com>
2017-01-19 15:47:53 +0800

    add redis 连接池功能

--- Libs/Controller.php
+++ Libs/Controller.php
@@ -6,9 +6,29 @@ class Controller{
     public $request = false;
     public $response = false; 
 
+    private $redis = false;
+
     function __construct($request,$response) {
+        global $config;
         $this->request = $request;
 	$this->response = $response;
+        if ( $config["redis"]["load"] ) {
+            $this->LoadRedis();
+        }
+    }
+
+    private function LoadRedis(){
+        global $config;
+	if ( $config["redis"]["coroutine"] ){
+            if (count($config["redis"]["coroutine_pool"]) == 0) {
+                $redis =  \WebWorker\Libs\Mredis::getInstance($config['redis']);
+                $config["redis"]["coroutine_pool"]->push($redis);
+                $config["redis"]["coroutine_count"]++;
+            }
+            $this->redis =  $config["redis"]["coroutine_pool"]->pop();
+	}else{
+	    $this->redis = \WebWorker\Libs\Mredis::getInstance($config['redis']); 
+	}
     }
 
     public function  ServerJson($data){
@@ -21,5 +41,13 @@ class Controller{
         $this->response->header("Content-Type", "text/html; charset=utf-8");
         $this->response->end($data);
     }
+    
+    public function __destruct() {
+        global $config;
+        if ( $this->redis && $config["redis"]["coroutine_pool"] ){
+            $config["redis"]["coroutine_pool"]->push($this->redis);
+        }
+    }
+
 
 }

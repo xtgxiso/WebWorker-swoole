8911f085b70f6aebde8ed5f8c6a3e8f71a3d9bda
xtgxiso <124960772@qq.com>
2017-01-21 15:31:31 +0800

    增加mysql协程自动重连

--- Libs/CoroutineMysql.php
+++ Libs/CoroutineMysql.php
@@ -4,25 +4,31 @@ namespace WebWorker\Libs;
 class CoroutineMysql{
 
     private $db = false;
+    private $config = false;
 
     public $affected_rows = 0;
     public $insert_id = 0;
 
     public function __construct($config=array()){
-        $host = isset($config["host"]) ? $config["host"] : "127.0.0.1";
-        $user = isset($config["user"]) ? $config["user"] : "root"; 
-	$password = isset($config["password"]) ? $config["password"] : "123456";
-	$database = isset($config["database"]) ? $config["database"] : "test";
-        $port = isset($config["port"]) ? $config["port"] : "3306";
-	$charset = isset($config["charset"]) ? $config["charset"] : "utf8";
-	$this->db = new \Swoole\Coroutine\MySQL();
-        $this->db->connect(array("host"=>$host,"user"=>$user,"password"=>$password,"database"=>$database,"port"=>$port,"timeout"=>3,"charset"=>$charset));
-	if ( $this->db->connect_error ){
-	    trigger_error('Connect Error: ' . $this->db->connect_error,E_USER_ERROR); 	
-	}
-	
+        $this->config = $config;
+	$this->connect();
     }
 
+    public function connect(){
+	$config = $this->config;
+        $host = isset($config["host"]) ? $config["host"] : "127.0.0.1";
+        $user = isset($config["user"]) ? $config["user"] : "root";
+        $password = isset($config["password"]) ? $config["password"] : "123456";
+        $database = isset($config["database"]) ? $config["database"] : "test";
+        $port = isset($config["port"]) ? $config["port"] : "3306";
+        $charset = isset($config["charset"]) ? $config["charset"] : "utf8";
+        $this->db = new \Swoole\Coroutine\MySQL();
+        $this->db->connect(array("host"=>$host,"user"=>$user,"password"=>$password,"database"=>$database,"port"=>$port,"timeout"=>3,"c
+harset"=>$charset));
+        if ( $this->db->connect_error ){
+            trigger_error('Connect Error: ' . $this->db->connect_error,E_USER_ERROR);
+        }
+    }
 
     public function query($sql){
 	$res = $this->db->query($sql);
@@ -41,6 +47,10 @@ class CoroutineMysql{
 	    }
 	    return $res;
 	}else{
+	    if ( !$this->db->connected ){
+	        $this->connect();
+		return $this->query($sql); 
+	    }
 	    return false;
 	}
     }
